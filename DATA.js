6102180e29da3cf140bb2fb08715e8ca2629fcd7
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-10 21:21:54 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "be1d2991-4339-41c9-94d4-f8b6506ccc52",
+    "home_feed": [
+        {
+            "id": "2f8c391d-715a-42e3-a4f4-753e886d1fad",
+            "artist_name": "story",
+            "home_carousel": [
+                {
+                    "id": "31818c6b-2722-4ef0-9575-c173651f9791",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/302.png"
+                },
+                {
+                    "id": "73f98803-57ac-4cfa-8adc-04bb2b99febf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/173.png"
+                },
+                {
+                    "id": "eee30847-2934-49b0-907b-96bafbc49fe6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/37.png"
+                },
+                {
+                    "id": "b8f183de-f507-4a0f-b252-ca547d8d4184",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/183.png"
+                },
+                {
+                    "id": "6f43585e-898d-4540-9cb0-512b05e86abe",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/191.png"
+                }
+            ],
+            "art_caption": "reason",
+            "art_description": "Data magazine time various. Window real their should sometimes take."
+        },
+        {
+            "id": "d11b4216-3ca3-4d05-90d8-150582fb4e7b",
+            "artist_name": "cut",
+            "home_carousel": [
+                {
+                    "id": "2a362352-33b7-48cf-9591-b7b0130a6e3d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/220.png"
+                },
+                {
+                    "id": "c8212f06-c587-4df1-9164-7fa4d0d9034c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/55.png"
+                },
+                {
+                    "id": "0b26a64d-43f9-492a-8510-e6f599b6c2a1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/173.png"
+                },
+                {
+                    "id": "59df6a27-e145-444e-9d45-09daa3fe141f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+                },
+                {
+                    "id": "71117442-d306-464d-878a-f64fe0358b58",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/179.png"
+                }
+            ],
+            "art_caption": "skin",
+            "art_description": "Price decision say take. Road wonder check."
+        },
+        {
+            "id": "2db54b43-53d9-48d3-ac70-6f3f95e7c9a7",
+            "artist_name": "may",
+            "home_carousel": [
+                {
+                    "id": "6bca46ef-48c7-4c35-b408-e11576f53c55",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/52.png"
+                },
+                {
+                    "id": "8ae891ef-daf9-49be-a72f-a9467bd59fb5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+                },
+                {
+                    "id": "97c19d17-c36d-4d4d-828e-53c6e4ee4504",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/251.png"
+                },
+                {
+                    "id": "f3accf9f-726d-4166-a120-1ef33919782d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                },
+                {
+                    "id": "57886095-2ecc-44d0-b650-3253f3521433",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/349.png"
+                }
+            ],
+            "art_caption": "along",
+            "art_description": "Issue admit arrive this establish kid spring. One skin age support possible since. Including them wait color."
+        },
+        {
+            "id": "ba007e48-06f7-47dd-9ca0-9c7097ec5ec1",
+            "artist_name": "design",
+            "home_carousel": [
+                {
+                    "id": "f59b2425-364f-4586-b030-f696b516e189",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/6.png"
+                },
+                {
+                    "id": "784730b2-5e70-45ed-a667-40b0e7035186",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/240.png"
+                },
+                {
+                    "id": "156667ab-bca5-4aa6-8e11-b1e8ce2a1ff1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/268.png"
+                },
+                {
+                    "id": "cd5495df-fdd9-4135-bba5-12174baf38ce",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/74.png"
+                },
+                {
+                    "id": "2ab9f440-b8d6-455c-9a6a-2085862b2eb4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/245.png"
+                }
+            ],
+            "art_caption": "it",
+            "art_description": "Style on plant wonder standard system. Police happen get experience. None my cup research consider teacher impact."
+        },
+        {
+            "id": "9443e545-fdb2-445c-9229-d52e4f6a19fd",
+            "artist_name": "they",
+            "home_carousel": [
+                {
+                    "id": "574387b1-b1be-4955-8c78-5cba50ac29a9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/202.png"
+                },
+                {
+                    "id": "27ee197d-1cd1-4072-9b53-0a31ee231d5e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/326.png"
+                },
+                {
+                    "id": "251d7da2-8f49-47b1-92da-bc33f83c54e6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png"
+                },
+                {
+                    "id": "00cf3c88-fd50-4bd2-9409-36e342236b4e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/173.png"
+                },
+                {
+                    "id": "63f17cdd-cff1-4875-b3e3-3f60af00be10",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/195.png"
+                }
+            ],
+            "art_caption": "win",
+            "art_description": "Need wish seem on. Must finish mention poor everyone spring stage."
+        }
+    ]
+}
